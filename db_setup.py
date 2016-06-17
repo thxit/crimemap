41b52bc9c018526cff05976c6f314e4327dd0f4a
thxit <1815078972@qq.com>
2016-06-17 21:19:58 +0800

    modified version

--- db_setup.py
+++ db_setup.py
@@ -1,25 +1,26 @@
 import pymysql
 import dbconfig
 
-connection = pymysql.connect(host='localhost',
+connection = pymysql.connect(host='127.0.0.1',
+			     port = 3406,
 			     user=dbconfig.db_user,
-			     passwd=db.db_passwd)
+			     passwd=dbconfig.db_password)
 
 try:
     with connection.cursor() as cursor:
 	sql = "CREATE DATABASE IF NOT EXISTS crimemap"
-	sursor.excute(sql)
-	sql = """CREATE TABLE IF NOT EXISTS crimap.crimes (
-    id int NOT NULL AUTO_INCREMENT,
-    latitude  FLOAT(10,6),
-    longitude FLOAT(10,6),
-    data DATATIME,
-    category varchar(50),
-    description varchar(1000),
-    updated_at TIMESTAMP,
-    PRIMARY KEY (id)
-    )"""
-	cursor.excute(sql)
+	cursor.execute(sql)
+	sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+id int NOT NULL AUTO_INCREMENT,
+latitude  FLOAT(10,6),
+longitude FLOAT(10,6),
+date DATETIME,
+category varchar(50),
+description varchar(1000),
+updated_at TIMESTAMP,
+PRIMARY KEY (id)
+)"""
+	cursor.execute(sql)
     connection.commit()
 finally:
     connection.close()
